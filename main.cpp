867f3ed626483fa4a69419f05039ffcfbdba7bcf
liameno <rhCWZ765hg@protonmail.com>
2022-06-21 23:26:31 +0300

    Refactoring & Optimization & Add set_random matrix, timer

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <algorithm>
+#include <chrono>
 
 #include "pagerank.h"
+
+using namespace std::chrono;
+
+inline void set_random(pagerank::matrix::matrix_t &matrix, const int size) {
+    matrix.resize(size);
+
+    for(int i = 0; i < size; ++i) {
+        matrix[i].resize(size);
+        for(int j = 0; j < size; ++j) {
+            matrix[i][j] = rand() % 2;
+        }
+    }
+}
 
 int main() {
     pagerank::matrix::matrix_t matrix = {
@@ -14,7 +28,12 @@ int main() {
             {1, 0, 0},*/
     };
 
+    //set_random(matrix, 1000);
+
+    auto start_time = high_resolution_clock::now();
     auto score = pagerank::compute(matrix);
+    auto end_time = high_resolution_clock::now();
+    std::cout << duration_cast<milliseconds>(end_time - start_time).count() << " ms" << std::endl;
 
     for (int i = 0; i < matrix.size(); i++) {
         std::cout << i << "-" << score[i] << std::endl;
--- pagerank.h
+++ pagerank.h
@@ -12,12 +12,12 @@ namespace pagerank {
 
         // Multiply matrix by scalar
         // Product stored in the matrix
-        void scalar_multiplication(matrix_t &matrix, int num_rows, int num_cols, double scalar) {
+        inline void scalar_multiplication(matrix_t &matrix, int num_rows, int num_cols, double scalar) {
             for (int i = 0; i < num_rows; i++)
                 for (int j = 0; j < num_cols; j++)
                     matrix[i][j] *= scalar;
         }
-        void scalar_multiplication(column_t &matrix, int num_rows, double scalar) {
+        inline void scalar_multiplication(column_t &matrix, int num_rows, double scalar) {
             for (int i = 0; i < num_rows; i++)
                 matrix[i] *= scalar;
         }
@@ -25,8 +25,8 @@ namespace pagerank {
         // Multiply matrix by column
         // Product stored in the column
         // Column must have num_cols rows
-        void column_multiplication(matrix_t &matrix, int num_rows, int num_cols, column_t &column) {
-            double product[num_cols];
+        inline void column_multiplication(matrix_t &matrix, int num_rows, int num_cols, column_t &column) {
+            std::vector<double> product(num_rows);
 
             for (int i = 0; i < num_rows; i++) {
                 double sum = 0;
@@ -38,20 +38,19 @@ namespace pagerank {
             }
 
             // Copy entries in the column
-            for (int i = 0; i < num_cols; i++)
-                column[i] = product[i];
+            column = product;
         }
 
         // Sum two matrices
         // Sum stored in the first matrix
         // Matrices must have the same dimensions
-        void addition(column_t &matrix1, column_t &matrix2, int num_rows) {
+        inline void addition(column_t &matrix1, column_t &matrix2, int num_rows) {
             for (int i = 0; i < num_rows; i++)
                 matrix1[i] += matrix2[i];
         }
 
         // Return Euclidean norm of column
-        double norm(column_t &column, int num_rows) {
+        inline double norm(column_t &column, int num_rows) {
             double sum = 0;
 
             for (int i = 0; i < num_rows; i++)
@@ -67,7 +66,6 @@ namespace pagerank {
         using namespace matrix;
         auto size = (int)link_matrix.size();
 
-
         for (int i = 0; i < size; ++i) {
             uint count = 0;
 
@@ -79,13 +77,15 @@ namespace pagerank {
 
             for (int j = 0; j < size; ++j)
                 if (link_matrix[j][i] == 1)
-                    link_matrix[j][i] = 1 / double(count);
+                    link_matrix[j][i] = 1 / (double)count;
         }
 
         // Initialize mean column and score column
-        column_t mean_column(size), score_column(size);
+        column_t mean_column(size);
+        column_t score_column(size);
+
         for (int i = 0; i < size; i++) {
-            double entry = 1 / (double) size;
+            auto entry = 1 / (double)size;
             mean_column[i] = entry;
             score_column[i] = entry;
         }
@@ -98,10 +98,7 @@ namespace pagerank {
 
         do {
             // Store score column before operations
-            column_t score_diff(size);
-
-            for (int i = 0; i < size; i++)
-                score_diff[i] = score_column[i];
+            column_t score_diff = score_column;
 
             // Multiply score column by weighted link matrix
             column_multiplication(link_matrix, size, size, score_column);
